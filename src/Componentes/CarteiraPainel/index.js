8a4d5a5e8bdd80f000250d0098acd9fee8a3aa13
Ivan Ramos <eng.ivanrff@gmail.com>
2022-11-13 20:13:39 -0300

    Puxando saldo do backend

--- src/Componentes/CarteiraPainel/index.js
+++ src/Componentes/CarteiraPainel/index.js
@@ -1,11 +1,26 @@
+import { useEffect } from "react";
+import { useState } from "react";
+import { getCarteiraByUsuarioId } from "../../Componentes/Service/ServiceCarteira";
 import { Link } from "react-router-dom";
 
 const CarteiraPainel = () => {
+
+    const [saldo, setSaldo] = useState("")
+
+    useEffect(() => {
+        getCarteiraByUsuarioId(11).then(
+            (response) =>{
+                setSaldo(response.qtdPauloCoin.toFixed(2).replace(".", ","))
+            }
+        ).catch();
+    }, []);
+
+
     return (
         <>
             <div id="resumo-carteira" className="box">
                 <div className="resumo-carteira-a">$PLC</div><div className="resumo-carteira-b"><Link to="/comprar-plc" className="text-link"><u>Adicionar Fundos</u></Link></div>
-                <div className="resumo-carteira-c">0,00000</div>
+                <div className="resumo-carteira-c">{saldo}</div>
             </div>
         </>
     )
